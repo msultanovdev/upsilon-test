8bddba5b5b83da10150001c70bd3fb650f597c25
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-04-04 01:12:54 +0500

    feat: setup redux store

---
+++ src/store/reducers/productSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { IProduct } from "../../types/types";
+type productSliceType = {
+  products: IProduct[];
+};
+
+const initialState: productSliceType = {
+  products: [],
+};
+
+export const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    setProducts(state, action) {
+      state = { ...state, products: action.payload };
+      return state;
+    },
+  },
+});
+
+export default productSlice.reducer;
