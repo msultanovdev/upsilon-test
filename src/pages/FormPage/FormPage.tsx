e2dcea45bac77aaf84a639c2993de03086e96cb1
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-04-04 15:10:11 +0500

    feat: add form layout

--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -1,5 +1,33 @@
+import { Form, Button } from "react-bootstrap";
+import cl from "./FormPage.module.css";
+
 const FormPage = () => {
-  return <div>Form Page</div>;
+  return (
+    <div className={cl.formContainer}>
+      <Form>
+        <Form.Group className="mb-2" controlId="formBasicName">
+          <Form.Label>Product name</Form.Label>
+          <Form.Control type="text" placeholder="Enter product name" />
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="formBasicDescription">
+          <Form.Label>Product description</Form.Label>
+          <Form.Control type="text" placeholder="Enter product description" />
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="formBasicPrice">
+          <Form.Label>Price</Form.Label>
+          <Form.Control type="numeric" placeholder="Price" />
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Опубликовать" />
+        </Form.Group>
+        <div className={cl.submitBtnWrapper}>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
 export default FormPage;
