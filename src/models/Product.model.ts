1b55c877b2b1e2d1f80f45101d4a31a9f2450b71
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-04-04 05:31:51 +0500

    feat: add unique id to Product model

--- src/models/Product.model.ts
+++ src/models/Product.model.ts
@@ -1,9 +1,13 @@
-export class Product {
+export default class Product {
+  public readonly id: string;
+
   constructor(
     public name: string,
     public price: number,
     public description: string,
     public published: boolean,
     public created_at: string,
-  ) {}
+  ) {
+    this.id = Date.now().toString();
+  }
 }
